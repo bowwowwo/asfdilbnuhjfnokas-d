3a2d031e2a7aae38924dc919824f687354456871
arnolds69 <122258184+arnolds69@users.noreply.github.com>
2024-04-24 21:59:51 +0300

    Update javasnake2.js

--- javasnake2.js
+++ javasnake2.js
@@ -4,6 +4,15 @@ button.addEventListener('click', function() {
     const result = window.confirm('Are you sure you want to proceed?');
 
     if (result) {
-                button.style.backgroundColor = 'red';
-    } else {}
+        const currentColor = button.style.backgroundColor;
+
+        
+        if (currentColor === 'red') {
+            button.style.backgroundColor = 'green';
+        } else {
+            button.style.backgroundColor = 'red';
+        }
+    } else {
+
+    }
 });
