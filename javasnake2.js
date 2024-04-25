7923aa4ba1ecaa3710263732cf10e0d4e21fc93f
wiyhs <96704375+wiyhs@users.noreply.github.com>
2024-04-25 21:31:46 +0300

    dahsboard, ieraksti un nepabeigta funkcija

--- javasnake2.js
+++ javasnake2.js
@@ -1,28 +1,46 @@
-const button = document.getElementById('vAButtons');
-
-button.addEventListener('click', function() {
-    const result = window.confirm('Vai esat pārliecināts?');
-
-    if (result) {
-        const currentColor = button.style.backgroundColor;
-
-        
-        if (currentColor === 'red') {
-            button.style.backgroundColor = 'green';
-        } else {
-            button.style.backgroundColor = 'red';
-        }
-    } else {
-
-    }
-});
+function poga1() {
+    var element = document.getElementById("vAButtons1");
+    if (element.style.backgroundColor == "red") {
+        element.style.backgroundColor = "green"}
+    else { element.style.backgroundColor = "red"}
+}
+function poga2() {
+    var element = document.getElementById("vAButtons2");
+    if (element.style.backgroundColor == "red") {
+        element.style.backgroundColor = "green"}
+    else { element.style.backgroundColor = "red"}
+}
+function poga3() {
+    var element = document.getElementById("vAButtons3");
+    if (element.style.backgroundColor == "red") {
+        element.style.backgroundColor = "green"}
+    else { element.style.backgroundColor = "red"}
+}
 
 function ieraksts() {
-
      var clickTime = new Date();
-    
      var formattedTime = clickTime.toLocaleString();
-    
-     console.log(User + "Button clicked at: " + formattedTime);
+     console.log("Button clicked at: " + formattedTime);
  }
 
+/* function popUps() {              //* KYS!!!!!!!!!!!!!!!!!!!!!!!!
+    
+
+    let buttonIsClicked = false;
+    vatslega.onclick('click', () =>{
+        buttonIsClicked = !buttonIsClicked;
+    })
+
+    if(buttonIsClicked === true){
+        alert("Izvēlēties šo atslēgu?");
+    }
+    else {
+        alert("Nodot šo atslēgu?");
+    }
+
+}
+
+function myFunc() {
+    popUps();
+    ieraksts();
+} */
